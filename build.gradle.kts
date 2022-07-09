07c016e500e5f4c0dbcb4dea0dbc85ab992e2b1a
Lucas <lucasvalenteds@gmail.com>
2022-07-09 16:55:59 -0300

    Adding Gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.springframework.boot") version "3.0.0-M3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    java
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://repo.spring.io/milestone")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
